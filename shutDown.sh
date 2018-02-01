2b3cc2b39e434ca0e24ef9e76e8d7a01c9ac4718
JagdCake <jc@jagdcake.com>
2018-02-01 16:28:09 +0200

    Show the shutdown date
    Also, show the startup time first and then the date.
    
    Resolves: #1

--- shutDown.sh
+++ shutDown.sh
@@ -4,11 +4,15 @@ kill -s SIGTERM `pidof tixati`; echo 'Exiting Tixati...'
 kill -s SIGTERM `pidof chrome`; echo 'Exiting Chrome...'
 kill -s SIGTERM `pidof steam`; echo 'Exiting Steam...'
 
-# double quotes evaluate any command between them (IF the command is between backticks) while single quotes print out a literal string
-echo "System powered on at: `uptime -s`" >> /home/jagdcake/Desktop/shutdown.txt;
+# split the start time and date into two variables
+starttime="`uptime -s | awk '{print $2}'`"
+startdate="`uptime -s | awk '{print $1}'`"
 
-# displays the current time + 30 seconds (hh:mm:ss) 
-echo "System shutdown at: `date +%H:%M:%S -d "+30 sec"`" >> /home/jagdcake/Desktop/shutdown.txt; 
+# double quotes evaluate any command between them (IF the command is between backticks) while single quotes print out a literal string
+echo "System powered on at: $starttime on $startdate" >> /home/jagdcake/Desktop/shutdown.txt;
+
+# displays the current time + 30 seconds (hh:mm:ss) and the date 
+echo "System shutdown at: `date +%H:%M:%S -d "+30 sec"` on `date +%Y-%m-%d`" >> /home/jagdcake/Desktop/shutdown.txt; 
 
 # using -e so echo recognizes escape characters
 echo -e "The system has been `uptime -p`\n" >> /home/jagdcake/Desktop/shutdown.txt
