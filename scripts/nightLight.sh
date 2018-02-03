6b95bd75f4349bd1343afa5bd8635eb18f3cafb1
JagdCake <jc@jagdcake.com>
2018-02-03 13:12:01 +0200

    Fix apps not triggering the script and improve performance
    Couldn't get the apps array working inside of a loop, replaced it with if/else statement. 'watch'-ing the script replaces the while loop, now the CPU usage is much lower.
    
    Resolves: #6
    Resolves: #7

--- scripts/nightLight.sh
+++ scripts/nightLight.sh
@@ -1,34 +1,24 @@
 #!/bin/bash
 
 # ====================
-# Solus specific (probably)
+# copy the script to the home dir, autostart (with 'watch -n 3' (update every 3 seconds)) in the background (with '&') and send the output to disappear inside 'null' (with '> /dev/null &') 
+# 'watch -n 3 ./nightLight.sh &> /dev/null &'
 
-# copy the script to home dir (cp ~/Documents/shellScripts/scripts/./nightLight.sh ~/) autostart the script in the background (./nightLight.sh &)
+# keep track of it with 'jobs'
 
-# keep track of it with 'ps aux | grep nightLight'
-
-# stop it with 'kill ID'
-
+# stop it with 'kill %Job ID'
 # ====================
 
-# applications to look for
-apps=(mpv hl2_linux CompanyOfHeroes2)
-arraylength=${#apps[@]}
+# check if any of the specified applications are currently running
+if [[ $(pgrep hl2_linux) || $(pgrep mpv) || $(pgrep CompanyOfHeroes2) ]]; then
+	check=1
+else
+	check=0
+fi
 
-# loop indefinitely (this doesn't seem good)
-while true;
-do
-# if any of the apps are running assign '1' to the var, if not assign '0' (only one app can run at a time)
-# iterate over the array items
-for (( i=0; i<${arraylength}; i++ ));
-do
-	check=`ps -ef | grep ${apps[$i]} | grep -v "grep" | wc -l`
-done
-
-# if one of the apps is running turn night-light off and vice versa
+# if any one of the apps is running turn night-light off and vice versa
 if [ "$check" -eq 1 ]; then
 	gsettings set org.gnome.settings-daemon.plugins.color night-light-enabled false
-elif [ "$check" -eq 0 ]; then
+else
 	gsettings set org.gnome.settings-daemon.plugins.color night-light-enabled true
 fi
-done
