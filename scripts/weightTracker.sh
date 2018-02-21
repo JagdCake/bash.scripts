a1443c8bb0f4459d59eb541ad5a938a8f927460f
JagdCake <jc@jagdcake.com>
2018-02-21 20:20:17 +0200

    Record the date too

--- scripts/weightTracker.sh
+++ scripts/weightTracker.sh
@@ -10,6 +10,7 @@
 
 # Format
 # Week: #
+# Date: ## Month ####
 # Weight: ##.# kg / ##.# lb
 # \n (newline)
 
@@ -19,9 +20,12 @@
 log=~/Desktop/weight.txt
 
 # get last week's number (it has to already be in the log file before you run the script for the first time)
-lastWeek=`tail -n 3 ~/Desktop/weight.txt | head -n 1 | awk '{printf $2}'`
+lastWeek=`tail -n 4 ~/Desktop/weight.txt | head -n 1 | awk '{printf $2}'`
 
 week=$(($lastWeek+1))
+
+# e.g. 21 Feb 2018
+date=`date "+%d %b %Y"`
 
 weightprompt () {
 # enter weight in format: ##.#, e.g. 78.3
@@ -41,6 +45,6 @@ done
 lb=0.45359
 
 # log the weight for the current week
-echo -e "Week: $week\nWeight: $kg kg / "$(echo "scale=1; $kg / $lb" | bc -l) lb"\n" >> $log
+echo -e "Week: $week\nDate: $date\nWeight: $kg kg / "$(echo "scale=1; $kg / $lb" | bc -l) lb"\n" >> $log
 
 # this part: "$(echo "scale=1; $kg / $lb" | bc -l)" converts the entered value into a floating-point lb value with the help of "bc -l" ("bc" allows float division and the "-l" flag prints the result as a float too, while "scale=1" limits the lb value to the first digit after the dot)
