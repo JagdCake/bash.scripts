03f155e1d5c5cb0ee0eeecce8f60aaeeb9a3cfa4
JagdCake <jc@jagdcake.com>
2018-07-24 16:03:29 +0300

    Add pomodoro script

---
+++ scripts/pomodoro.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# https://en.wikipedia.org/wiki/Pomodoro_Technique
+
+# if spd-say sound is crackling, open '/usr/share/defaults/speech-dispatcher/speechd.conf' and change 'AudioOutputMethod' to 'libao'
+
+pomodoro_time=25
+
+checkmarks=0
+
+timer() {
+    target=$(date +%M -d "+${pomodoro_time} minutes")
+    current=$(date +%M)
+    passed=0
+
+    while [ $target -ne $current ]; do
+        sleep 5m
+        echo "$(echo $passed + 5 | bc -l ) minutes have passed"
+        passed=$(($passed+5))
+        current=$(date +%M)
+    done
+
+    spd-say -t female1 'stop'
+    checkmarks=$(($checkmarks+1))
+
+    if [ $checkmarks -ne 4 ]; then
+        echo '✓'
+        sleep 5m
+        spd-say -t female1 'start'
+        timer
+    else
+        echo '✓'
+        echo 'Take a 20 minute break'
+        sleep 20m
+        checkmarks=0
+        spd-say -t female1 'start'
+        timer
+    fi
+}
+
+timer
+
