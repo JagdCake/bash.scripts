392055ac7ba43a9ecfae87e8ffcaaa9a59731ea7
JagdCake <jc@jagdcake.com>
2018-07-30 11:35:53 +0300

    Add usage and options sections

--- scripts/night_light.sh
+++ scripts/night_light.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
-# ====================
-# copy the script to the home dir, start (with 'watch -n 3' (update every 3 seconds)) in the background (with '&') and send the output to disappear inside 'null' (with '> /dev/null &') 
-# "watch -n 3 ./night_light.sh &> /dev/null &"
+### Usage ###
+# Create an alias for the script in '.bashrc' and run it from a terminal
+    # alias togglenightl='watch -n [x] /path/to/./night_light.sh &> /dev/null &'
+        # 'watch -n [x]' runs the script every x seconds, for example - 3 
+        # '&> /dev/null &' redirects the output, so the script can run in the background
+    # $ togglenightl
+# Keep track of it with 'jobs' (have to be in the same terminal)
+    # stop it by closing the shell / killing it with 'kill %[jobs ID]'
+### ###
 
-# keep track of it with "jobs" (have to be in the same shell)
-
-# stop it by closing the shell / killing it with "kill %[jobs ID]"
-# ====================
-
+### Options ###
 # set your screen resolution
 res_width=1920
 res_height=1080
+### ###
 
 # get the id of the active window
 active_window="`xprop -root _NET_ACTIVE_WINDOW | awk -F'#' '{print $2}'`"
