b917ce3d2412aade4ac71bdf47e4f7d2c8bb9333
JagdCake <jc@jagdcake.com>
2018-07-30 07:21:11 +0300

    Add an option for setting workspace names

--- scripts/workspace.sh
+++ scripts/workspace.sh
@@ -5,3 +5,8 @@
 # Run any specified commands in that new workspace, but only if it's inactive (no running apps)
 # Change the background depending on the workspace
 ### ###
+
+### Options ###
+workspace_names=(Home Work Read Movie CTRL)
+### ###
+
