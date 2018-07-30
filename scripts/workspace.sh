22a2de4cc8e4e956a907cb1fb17a1553e9b3e553
JagdCake <jc@jagdcake.com>
2018-07-30 07:15:49 +0300

    Add a script for workspace switching

---
+++ scripts/workspace.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+### Description ###
+# Choose a workspace from a pre-defined list and switch to it
+# Run any specified commands in that new workspace, but only if it's inactive (no running apps)
+# Change the background depending on the workspace
+### ###
