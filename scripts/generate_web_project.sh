7c9f2de73b9270eaba7ac9da68899f5c2b12c55b
JagdCake <jc@jagdcake.com>
2018-07-31 09:55:05 +0300

    Add a script for generating web projects

---
+++ scripts/generate_web_project.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+### Description ###
+# Choose the type of web project you're starting (Node.js app, WordPress website, static site, etc.)
+# Enter a project name and create a directory of the same name
+# Copy template files and folders from a predefined location to the project dir
+# Install packages, initialize a git repository, add files / folders to .gitignore
+### ###
