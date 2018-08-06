0e0801b86168411b99ce1a018c0781ca81f3e0cf
JagdCake <jc@jagdcake.com>
2018-08-06 12:19:56 +0300

    Add option to choose when to log the weight

--- scripts/weight_tracker.sh
+++ scripts/weight_tracker.sh
@@ -20,6 +20,11 @@
 ### Options ###
 # log file location
 log=~/Desktop/weight.txt
+
+declare -A days
+days=([Monday]=1 [Tuesday]=2 [Wednesday]=3 [Thursday]=4 [Friday]=5 [Saturday]=6 [Sunday]=7)
+# choose a day for logging your weight
+day=${days['Monday']}
 ### ###
 
 # Log format
