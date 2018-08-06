86f03fb1324e37bf629d1b05236da427f3385ad3
JagdCake <jc@jagdcake.com>
2018-08-06 12:17:24 +0300

    Recreate the weight prompt function

--- scripts/weight_tracker.sh
+++ scripts/weight_tracker.sh
@@ -53,18 +53,30 @@ show_weight_change_since() {
     fi
 }
 
+# e.g. 21 Feb 2018
+date=$(date "+%d %b %Y")
 
-# accept data only if it's formatted properly
-# regex is checking for any number followed by a dot and ending with a single digit
-while [[ ! "$kg" =~ [0-9]\.[0-9]{1}$ ]]; do
-	echo "Wrong format. Use: ##.#"
-	weightprompt
-done
+weight_prompt() {
+    # enter weight in format: ##.#, e.g. 78.3
+    read -p "Weight: " kg
 
-lb=0.45359
+    # accept data only if it's formatted properly
+    # regex is checking for any number followed by a dot and ending with a single digit
+    while [[ ! "$kg" =~ [0-9]\.[0-9]{1}$ ]]; do
+        echo "Wrong format. Use: ##.#"
+        weightprompt
+    done
 
-# log the weight for the current week
-echo -e "Week: $week\nDate: $date\nWeight: $kg kg / "$(echo "scale=1; $kg / $lb" | bc -l) lb"\n" >> "$log"
+    # get last week's number (it has to already be in the log file before you run the script for the first time)
+    last_week=$(tail -n 4 "$log" | head -n 1 | awk '{ print $2 }')
 
-# this part = "$(echo "scale=1; $kg / $lb" | bc -l)" converts the entered value into a floating-point lb value with the help of "bc -l" ("bc" allows float division and the "-l" flag prints the result as a float too, while "scale=1" limits the lb value to the first digit after the decimal point)
+    week=$(($last_week+1))
+
+    # log the weight for the current week
+    echo -e "Week: $week\nDate: "$date"\nWeight: $kg kg / "$(echo "scale=1; $kg / $lb" | bc -l) lb"\n" >> "$log"
+
+    # this part = "$(echo "scale=1; $kg / $lb" | bc -l)" converts the entered value into a floating-point lb value with the help of "bc -l" ("bc" allows float division and the "-l" flag prints the result as a float too, while "scale=1" limits the lb value to the first digit after the decimal point)
+}
+
+
 
