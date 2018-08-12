4a7d7c5d2eec5ad9329e01631b2d0283b62727f2
JagdCake <jc@jagdcake.com>
2018-08-12 11:11:56 +0300

    Hide errors

--- scripts/check_websites.sh
+++ scripts/check_websites.sh
@@ -20,12 +20,12 @@ websites=(https://modeling.jagdcake.com/ https://dreams.jagdcake.com/)
 
 for i in ${websites[@]}
 do
-    status_code=$(curl -Is ${i} | head -n 1 | awk '{ print $2 }')
+    status_code=$(curl -Is ${i} | head -n 1 | awk '{ print $2 }' )
     if [ $status_code -eq 200 ]
         then
             echo -e "${i} ONLINE"
         else
             echo -e "${i} OFFLINE"
-    fi
+    fi 2>/dev/null
 done
 
