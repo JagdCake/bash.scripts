07fcbc28fede45a8ad0f66a178a2b2ead65ab9ae
JagdCake <jc@jagdcake.com>
2018-09-03 12:25:51 +0300

    Create log file if it doesn't exist

--- scripts/weight_tracker.sh
+++ scripts/weight_tracker.sh
@@ -30,6 +30,10 @@ days=([Monday]=1 [Tuesday]=2 [Wednesday]=3 [Thursday]=4 [Friday]=5 [Saturday]=6 
 day=${days['Monday']}
 ### ###
 
+if [ ! -e "$log" ]; then
+    touch "$log"
+fi
+
 lb=0.45359
 
 show_weight_change_since() {
