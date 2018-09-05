2d2844dd9a6398b245ab7e7fbb6a9718fa7c8d4a
JagdCake <jagdcake@outlook.com>
2018-09-05 17:38:56 +0300

    Add description

--- scripts/build_web_project.sh
+++ scripts/build_web_project.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+# create a different function for every part of the build process (minify CSS, optimize images, etc.)
+    # one function for static and dynamic sites
+# select between project types (static_firebase, dynamic_node, dynamic_wordpress)
+# choose between building from scratch (run all functions) and building piece by piece (select function to run) 
 
