3303443d932d4793c17e9331001c0b5c6cdfbe60
JagdCake <jagdcake@outlook.com>
2018-09-07 13:41:33 +0300

    Add function to choose a project directory

--- scripts/build_web_project.sh
+++ scripts/build_web_project.sh
@@ -5,3 +5,13 @@
 # select between project types (static_firebase, dynamic_node, dynamic_wordpress)
 # choose between building from scratch (run all functions) and building piece by piece (select function to run) 
 
+choose_project() {
+    read -e -p "Enter project directory: " project_dir
+
+    if [ ! -d "$project_dir" ]; then
+        echo "Directory doesn't exist!"
+        choose_project
+    fi
+}
+
+
