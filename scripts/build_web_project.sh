c3e2b9316e7835fcd0f7f4631a545f3e69a61c7b
JagdCake <jagdcake@outlook.com>
2018-09-07 14:17:30 +0300

    Trim whitespace

--- scripts/build_web_project.sh
+++ scripts/build_web_project.sh
@@ -3,7 +3,7 @@
 # create a different function for every part of the build process (minify CSS, optimize images, etc.)
     # one function for static and dynamic sites
 # select between project types (static_firebase, dynamic_node, dynamic_wordpress)
-# choose between building from scratch (run all functions) and building piece by piece (select function to run) 
+# choose between building from scratch (run all functions) and building piece by piece (select function to run)
 
 choose_project() {
     read -e -p "Enter project directory: " project_dir
