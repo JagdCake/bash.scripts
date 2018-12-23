7aa611810d15908da07ea8ec1ffda4f2cf7eb6bf
JagdCircle <circlycircle@gmail.com>
2018-12-23 12:23:54 +0200

    Add script for tracking time

---
+++ scripts/track_time.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# choose what to track with an argument — learn, work, workout
+
+# select what you want to do
+# start / stop tracking time; show a summary; options to correct tracked time; quit
+
